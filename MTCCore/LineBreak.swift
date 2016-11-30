2179451313f1d8a3d0dbe24dacdd6ac99764ac4f
Akira Hayashi <akirark@users.noreply.github.com>
2016-11-30 19:33:50 +0900

    Apply API Design Guidelines to LineBreak class

--- MTCCore/LineBreak.swift
+++ MTCCore/LineBreak.swift
@@ -15,22 +15,17 @@
 
 import Foundation
 
-public enum LineBreakType {
-    case Unknown
-    case LF
-    case CR
-    case CRLF
-    case LS
-}
-
-private let kLineBreakCharacterCR: Character = "\r"
-private let kLineBreakCharacterLF: Character = "\n"
-private let kLineBreakCharacterCRLF: Character = "\r\n"
-private let kLineBreakCharacterLS: Character = "\u{2028}"
-
 /// The LineBreak class detects and replaces line breaks
 public final class LineBreak {
-    
+
+    /// Line break types
+    public enum `Type`: Character {
+        case lf = "\n"
+        case cr = "\r"
+        case crlf = "\r\n"
+        case ls = "\u{2028}"
+    }
+
     public init() {
         
     }
@@ -38,70 +33,33 @@ public final class LineBreak {
     /// Detects the line break type of the text
     /// \param text The text to be checked
     /// \return Detected line break type
-    public static func detectLineBreakInText(_ text: String) -> LineBreakType {
-        var ret = LineBreakType.Unknown
-        var prevCharacter: Character = "\0"
-        
+    public static func lineBreakType(inText text: String) -> Type? {
+
         for c in text.characters {
-            switch c {
-            case kLineBreakCharacterCR:
-                ret = .CR
-            case kLineBreakCharacterLF:
-                ret = .LF
-            case kLineBreakCharacterCRLF:
-                ret = .CRLF
-            case kLineBreakCharacterLS:
-                ret = .LS
-            default:
-                break
+            if let type = Type(rawValue: c) {
+                return type
             }
-            
-            if ret != .Unknown {
-                break
-            }
-            
-            prevCharacter = c
         }
-        
-        if ret == .Unknown && prevCharacter == kLineBreakCharacterCR {
-            ret = .CR
-        }
-        
-        return ret
+
+        return nil
     }
-    
+
     /// Replaces line breaks
     /// \param text The text
     /// \param type The new line break type
     /// \return The new text
-    public static func replaceLineBreaksInText(_ text: String, type: LineBreakType) throws -> String {
-        var ret: String = ""
-        
-        let lbc: Character
-        switch type {
-        case .CR:
-            lbc = kLineBreakCharacterCR
-        case .LF:
-            lbc = kLineBreakCharacterLF
-        case .CRLF:
-            lbc = kLineBreakCharacterCRLF
-        case .LS:
-            lbc = kLineBreakCharacterLS
-        default:
-            throw MTCError.InvalidLineBreakType
-        }
-        
+    public static func replaceLineBreaks(inText text: String, to type: Type) -> String {
+        let lbc = type.rawValue
+        var replacedText = ""
+
         for c in text.characters {
-            if c == kLineBreakCharacterCR ||
-                c == kLineBreakCharacterLF ||
-                c == kLineBreakCharacterCRLF ||
-                c == kLineBreakCharacterLS {
-                ret.append(lbc)
+            if let _ = Type(rawValue: c) {
+                replacedText.append(lbc)
             } else {
-                ret.append(c)
+                replacedText.append(c)
             }
         }
         
-        return ret
+        return replacedText
     }
 }
